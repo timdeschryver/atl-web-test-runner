0d060d389f61fa292c1c2d9433345e41495ca193
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2024-01-20 11:24:07 +0100

    counter example using Web Test Runner

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { expect } from '@esm-bundle/chai';
 import { render, screen } from '@testing-library/angular';
 import { userEvent } from '@testing-library/user-event';
 import { AppComponent } from './app.component';
@@ -7,17 +8,17 @@ it('renders and interacts with counter', async () => {
   await render(AppComponent);
 
   const div = screen.getByTestId('count');
-  expect(div).not.toBeNull();
+  expect(div).to.not.equal(null);
 
-  expect(div.textContent).toBe('0');
+  expect(div.textContent).equal('0');
 
   await user.click(screen.getByRole('button', { name: /increment/i }));
   await user.click(screen.getByRole('button', { name: /increment/i }));
-  expect(div.textContent).toBe('2');
+  expect(div.textContent).to.equal('2');
 
   await user.click(screen.getByRole('button', { name: /decrement/i }));
-  expect(div.textContent).toBe('1');
+  expect(div.textContent).to.equal('1');
 
   await user.click(screen.getByRole('button', { name: /reset/i }));
-  expect(div.textContent).toBe('0');
+  expect(div.textContent).to.equal('0');
 });
